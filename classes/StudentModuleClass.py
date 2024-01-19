052693b95ac07cea4c895b51cedd500d175b3110
tobystatonbevan <154239390+tobystatonbevan@users.noreply.github.com>
2024-01-19 18:27:19 +0000

    Improved week numbering and exception handling

--- classes/StudentModuleClass.py
+++ classes/StudentModuleClass.py
@@ -7,18 +7,30 @@ class Module:
         self.__weeks = weeks
 
     def createModuleRoot(self):
-        os.mkdir(self.__name)
+        try:
+            os.mkdir(self.__name)
+        except:
+            raise
 
     def createWeeks(self):
-        os.chdir(os.path.join(self.__destinationDir,self.__name))
-        for i in range(self.__weeks):
-            os.mkdir(f"Week {i+1}")
+        try:
+            os.chdir(os.path.join(self.__destinationDir,self.__name))
+            for i in range(self.__weeks):
+                if i < 9:
+                    os.mkdir(f"Week 0{i+1}")
+                else:
+                    os.mkdir(f"Week {i+1}")
+        except:
+            raise
 
     def createContent(self):
-        os.chdir(os.path.join(self.__destinationDir,self.__name))
-        for i in os.listdir(os.path.join(self.__destinationDir,self.__name)):
-             os.chdir(i)
-             os.mkdir("Lectures")
-             os.mkdir("Labs")
-             os.mkdir("Resources")
-             os.chdir(os.path.join(self.__destinationDir,self.__name))
+        try:
+            os.chdir(os.path.join(self.__destinationDir,self.__name))
+            for i in os.listdir(os.path.join(self.__destinationDir,self.__name)):
+                os.chdir(i)
+                os.mkdir("Lectures")
+                os.mkdir("Labs")
+                os.mkdir("Resources")
+                os.chdir(os.path.join(self.__destinationDir,self.__name))
+        except:
+            raise
